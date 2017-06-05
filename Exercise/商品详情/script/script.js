e8c7b8c52efa631636c95d8db7518d6f940fd121
Your Real Name <x.boy724145155@gmail.com>
2017-06-05 09:15:01 +0800

    完整版

--- Exercise/商品详情/script/script.js
+++ Exercise/商品详情/script/script.js
@@ -4,65 +4,118 @@
 // var imgUrl = $('.detail_left')[0].getElementsByTagName('img')[0].src;
 // var img1 = $('.contain')[0].getElementsByTagName('img')[0];
 // var img2 = $('.contain')[0].getElementsByTagName('img')[1];
-$(function () {
-        let commodityId = 2;
-        let url = 'http://duolaimon.cn/';
-        $.ajax({
-            url: url + 'shop/commodities/' + commodityId,
-            type: "GET",
-            // contentType: "application/json",
-            // data: "",
-            // crossDomain: true,
-            success: function (result) {
-                console.log('success!');
-                $('.price')[0].getElementsByTagName("b")[0].innerHTML = '￥' + result.commodityPrice;
-                $('.price')[0].getElementsByTagName('i')[0].innerHTML = result.commodityLeavenum;
-                $('.integral')[0].getElementsByTagName('span')[0].innerHTML = result.commodityPrice / 10;
-                $(".detail_left")[0].getElementsByTagName("img")[0].src = url + result.commodityPicture;
-                $('.detail_left')[0].getElementsByTagName('a')[0].href = url + result.commodityPicture;
-                $('.spec')[0].getElementsByTagName('a')[0].innerHTML = result.commodityStandard + 'kg';
-                /*
-                 * sessionStorage 最近浏览
-                 **/
+let price = $('.price')[0];
+let intergral = $('.integral')[0];
+let detail_left = $(".detail_left")[0];
+let contain = $('.contain')[0];
+let spec = $('.spec')[0];
 
-                let imgUrl = $('.detail_left')[0].getElementsByTagName('img')[0].src;
-                let img1 = $('.contain')[0].getElementsByTagName('img')[0];
-                let img2 = $('.contain')[0].getElementsByTagName('img')[1];
-                if (sessionStorage.length) {
-                    sessionStorage[1] = sessionStorage[0];
-                }
-                sessionStorage[0] = imgUrl;
-                img1.src = sessionStorage[0];
-                img2.src = sessionStorage[1];
+    $(function () {
+            let commodityId = 77;
+            let url = 'http://duolaimon.cn/';
+            $.ajax({
+                url: url + 'shop/commodities/' + commodityId,
+                type: "GET",
+                // contentType: "application/json",
+                // data: "",
+                // crossDomain: true,
+                success: function (result) {
+                    console.log('success!');
+                    price.getElementsByTagName("b")[0].innerHTML = '￥' + result.commodityPrice;
+                    price.getElementsByTagName('i')[0].innerHTML = result.commodityLeavenum;
+                    intergral.getElementsByTagName('span')[0].innerHTML = result.commodityPrice / 10;
+                    detail_left.getElementsByTagName("img")[0].src = url + result.commodityPicture;
+                    detail_left.getElementsByTagName('a')[0].href = url + result.commodityPicture;
+                    spec.getElementsByTagName('a')[0].innerHTML = result.commodityStandard + 'kg';
 
+                    /*
+                     * sessionStorage 最近浏览
+                     **/
 
-            },
-            error: function () {
-                console.log('ajax error');
-            },
-
-        });
-    }
-);
+                    let imgUrl = detail_left.getElementsByTagName('img')[0].src;
+                    let img1 = contain.getElementsByTagName('img')[0];
+                    let img2 = contain.getElementsByTagName('img')[1];
+                    if (sessionStorage.length) {
+                        sessionStorage[1] = sessionStorage[0];
+                    }
+                    sessionStorage[0] = imgUrl;
+                    img1.src = sessionStorage[0];
+                    img2.src = sessionStorage[1];
 
 
+                },
+                error: function () {
+                    console.log('ajax error');
+                },
+
+            });
+        }
+    );
+
+let number = $('#num')[0];
 function sub() {
-    let goodsValue = parseInt($('#num')[0].value);
+    let goodsValue = parseInt(number.value);
     if (goodsValue > 0) {
         goodsValue -= 1;
     }
     else {
         goodsValue = 0;
     }
-    $('#num')[0].value = goodsValue;
+    number.value = goodsValue;
 }
 function add() {
-    let goodsValue = parseInt($('#num')[0].value);
+    let goodsValue = parseInt(number.value);
     if (goodsValue || goodsValue === 0) {
         goodsValue += 1;
     }
     else {
         goodsValue = 0;
     }
-    $('#num')[0].value = goodsValue;
+    number.value = goodsValue;
+}
+
+
+function GetQueryString(name) {
+    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)","i");
+    let r = window.location.search.substr(1).match(reg);
+    if (r!==null) return (r[2]); return null;
+}
+let commidityid;
+if(GetQueryString("id")!==null)
+{
+    commodityid = decodeURIComponent(GetQueryString("id"));
+    
+}
+
+function addCar() {
+    $.ajax({
+        type: "POST",
+        url: '/kys24/userController/login?userPhone=152010&userPassword=123456789' ,
+        data: '',
+        // dataType: "json",
+        // crossDomain: true,
+        success: function (result) {
+            if(result.info === 1){
+                console.log('success');
+                $.ajax({
+                    type: 'POST',
+                    url: '/kys24/cart/commodities/'+commodityid,
+                    data: '',
+                    // dataType: 'jsonp',
+                    crossDomain: true,
+                    success: function (msg) {
+                        alert('加入购物车成功！');
+                    },
+                    error: function () {
+                        alert('加入购物车失败！');
+                    }
+                });
+            }
+            else{
+                alert('please sign in before!');
+            }
+        },
+        error: function () {
+        }
+    });
 }
